8b5cd8d68a5792b7c7cf00e450159021ecc5b6e2
tanting178 <tanting.2016@gmail.com>
2025-07-18 16:11:20 +0800

    Updated llm.py to read key from streamlit

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -1,12 +1,22 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+import streamlit as st
 import tiktoken
 
-load_dotenv('.env')
+# load_dotenv('.env')
 
 # Pass the API Key to the OpenAI Client
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+#client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+if load_dotenv('.env'):
+   # for local development
+   OPENAI_KEY = os.getenv('OPENAI_API_KEY')
+else:
+   OPENAI_KEY = st.secrets['OPENAI_API_KEY']
+
+# Pass the API Key to the OpenAI Client
+client = OpenAI(api_key=OPENAI_KEY)
 
 
 # Function for Generating Embedding
